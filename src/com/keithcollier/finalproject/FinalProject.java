363786e6afa3828803ab4dbdc2c091b92dee0c7b
Unknown <tockstaunt@gmail.com>
2018-03-19 04:29:13 -0500

    fixed complete

--- src/com/keithcollier/finalproject/FinalProject.java
+++ src/com/keithcollier/finalproject/FinalProject.java
@@ -22,21 +22,21 @@ public static void main(String[] args) throws Exception{
 	
 	//TaskList
 	List<String> taskItems = new ArrayList<String>(25);
-	
-	//for loop to display list
-	for (int i = 0; i < menuItems.size();i++) {
-		System.out.println(menuItems.get(i));
-		
-	}
-	
 	boolean quit = false;
-	int menu;
+	
 	
 	//switches
 	do {	
 		
-		System.out.println("What would you like to do: ");
 		
+		//for loop to display list
+		for (int i = 0; i < menuItems.size();i++) {
+			System.out.println(menuItems.get(i));
+			
+		}
+		
+		System.out.println("What would you like to do: ");
+		int menu;
 		menu = in.nextInt();
 	
 		switch (menu) {	
@@ -48,6 +48,7 @@ public static void main(String[] args) throws Exception{
 			taskItems.add(input);
 			System.out.print("You have added a task!");			
 			break;
+			
 		//case 2 when two is selected allows for deleting tasks	
 		case 2:
 			int numbers2 = 1;
@@ -60,7 +61,7 @@ public static void main(String[] args) throws Exception{
 	    			
 	    		}
 			System.out.println("What Task would you like to delete?");
-			//section for the logic around deleting tasks
+			//the logic around deleting tasks
 			int input2 = in.nextInt();
 			
 			if(taskItems.get(input2).isEmpty()){
@@ -68,7 +69,7 @@ public static void main(String[] args) throws Exception{
 			}
 			else {
 				
-				taskItems.remove(input2);
+				taskItems.remove(input2) ;
 				System.out.print("You have deleted a task!");
 			}
 			
@@ -76,6 +77,7 @@ public static void main(String[] args) throws Exception{
             break;
 
 		case 3:
+			//displays list of task to make as complete
 			int numbers3 = 1;
 	    	  	  
 	    	for (int i = 0; i < taskItems.size();i++) {
@@ -83,8 +85,14 @@ public static void main(String[] args) throws Exception{
 	    	  String listItems = numbers3 +": " + taskItems.get(i);
 	    	  numbers3++;
 	    	  System.out.println(listItems);
-	    			
 	    	}
+	    	
+	    	//logic of adding complete
+	    	int input3 = in.nextInt();
+	    	
+	    	 String taskComp = taskItems.get(input3) + " (COMPLETE)";
+	    	 taskItems.set(input3, taskComp );
+	    	
             break;
 
       case 4:
@@ -99,7 +107,7 @@ public static void main(String[] args) throws Exception{
     			System.out.println(listItems);
     			
     		}
-    	 
+    	 System.out.println("End of List");
             break;
 		
 	case 0:
